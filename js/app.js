7801deec921f57d076a9ecefa2fb45e10ce61c4b
alexander215 <alexander.hoskinson@colorado.edu>
2019-07-02 16:27:22 -0700

    Basic start function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log('Hi!');
+$('#startButton').on('click', (e) => {
+    startGame();
+}
+)
+
+startGame = () => {
+    const namePrompt= prompt('What do you want to name your pet?', '');
+    $('header').append('Hello ', namePrompt);
+}
