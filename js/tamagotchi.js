0149c924c80a8071df919e4964a160a2df3c187d
alexander215 <alexander.hoskinson@colorado.edu>
2019-07-02 11:53:40 -0700

    Created Tamagotchi class

---
+++ js/tamagotchi.js
@@ -0,0 +1,11 @@
+class Tamagotchi {
+    constructor(name){
+        this.name = name;
+        this.hunger = 0;
+        this.sleepiness = 0;
+        this.boredom = 0;
+        this.age = 0;
+
+    }
+
+}
