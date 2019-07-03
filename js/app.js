d81871b0e7a0405a55590e64e9efc52f44e44e60
alexander215 <alexander.hoskinson@colorado.edu>
2019-07-02 17:52:20 -0700

    Input name of the pet

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
+let petName;
+
 $('#startButton').on('click', (e) => {
-    startGame();
+    game.startGame();
 }
 )
+const game = {
+startGame () {
+    petName = prompt('What do you want to name your pet?', '');
+    // Tamagotchi(namePrompt);
+    console.log(petName);
+    $('#startButton').hide();
+    petName = new Tamagotchi(petName);
+    console.log(petName.name);
+    // petName.initPet();
+    // console.log(Tamagotchi.name);
+}
+feed (){
+        
+}
 
-startGame = () => {
-    const namePrompt= prompt('What do you want to name your pet?', '');
-    $('header').append('Hello ', namePrompt);
+
 }
--- js/tamagotchi.js
+++ js/tamagotchi.js
@@ -1,11 +1,19 @@
 class Tamagotchi {
-    constructor(name){
-        this.name = name;
+    constructor(petName){
+        this.name = petName;
         this.hunger = 0;
         this.sleepiness = 0;
         this.boredom = 0;
         this.age = 0;
 
     }
+    initPet(){
+        console.log(this.name, "<--initpet");
+        // const $h1 = $
+    }
+    hunger(){
+        // every 10 seconds increase hunger by 1 point
+    }
+
 
 }
